75caa507e2ed6ff6ea0a79d1197c200d09a6e70f
ThunderDd <dedeguay@gmail.com>
2023-05-04 22:38:35 -0400

    Fixed deployment issues

--- src/app/(pages)/(public)/layout.tsx
+++ src/app/(pages)/(public)/layout.tsx
@@ -1,7 +1,7 @@
-import type { LayoutComponent } from "@/utils/types";
+import type { LayoutComponent } from "~/utils/types";
 
 const PublicLayout: LayoutComponent = ({ children }) => {
-    return <div>{children}</div>;
+	return <div>{children}</div>;
 };
 
 export default PublicLayout;
